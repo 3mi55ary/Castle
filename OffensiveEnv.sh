c8bcc7a8c7e85d301f038ce394dcabaf68243e03
3mi55ary <Gapedwards4@gmail.com>
2025-08-28 14:31:32 -0400

    OffensiveEnv Start
    OffensiveEnv headers and concept built

---
+++ OffensiveEnv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#===============================================================================
+# SCRIPT NAME   : OffensiveEnv.sh
+# DESCRIPTION   : Builds an offensive toolkit tailored to Windows
+# AUTHOR        : 3mi55ary
+# DATE          : 2025-08-28
+# VERSION       : 1.0
+# USAGE         : ./OffensiveEnv.sh [options]
+# NOTES         : Tested on Latest Release of Kali Linux
+#===============================================================================
