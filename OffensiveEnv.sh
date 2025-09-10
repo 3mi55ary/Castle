b5f0349b6cdbcb6b8b4558bf26723c720935643c
3mi55ary <Gapedwards4@gmail.com>
2025-09-10 17:20:03 -0400

    Update OffensiveEnv.sh

--- OffensiveEnv.sh
+++ OffensiveEnv.sh
@@ -12,8 +12,7 @@
 #===============================================================================
 # System Basics ================================================================
 #===============================================================================
- #sshpass
- #https://github.com/insidetrust/statistically-likely-usernames
+#sshpass
 # Prepare System
 sudo apt update
 
@@ -23,50 +22,81 @@ mkdir -p ~/Captures ~/WindowsTools ~/PivotingTools ~/Loot
 # PimpMyKali Addition
 cd ~
 git clone https://github.com/Dewalt-arch/pimpmykali
-cd ~/pimpmykali
-# WORKING HERE ON AUTOMATING PIMPMYKALI FROM CLI --------------------------------------------------- >>> ###
-sudo ./pimpmykali.sh --auto
 
+# Ensure Python3 + pipx are Installed
+if ! command -v python3 &>/dev/null; then
+    echo "[*] Installing Python3..."
+    sudo apt install -y python3 python3-pip
+fi
+
+if ! command -v pipx &>/dev/null; then
+    echo "[*] Installing pipx..."
+    sudo apt install -y pipx
+    pipx ensurepath
+fi
 #===============================================================================
 # Screenshots / Captures =======================================================
 #===============================================================================
-# Install and Configure Flameshot for Instant Usage
+# Install and Configure Termshot
+
+# Install and Configure Flameshot for Instant Usage (BACKUP)
 sudo apt install -y flameshot
 flameshot &
 flameshot gui --path "$HOME/Captures" --accept-on-select &
 
-# Set XFCE's default screenshot save path (BACKUP)
+# Set XFCE's default screenshot save path (BACKUP BACKUP)
 xfconf-query -c xfce4-screenshooter \
     -p /last-save-location \
     -s "$HOME/Captures"
+
+#===============================================================================
+# Passwords ====================================================================
+#=============================================================================== 
+# hashcat
+# seclists
+# rockyou extract
+# https://github.com/insidetrust/statistically-likely-usernames
 
 #===============================================================================
 # WINDOWS TOOLING ==============================================================
 #===============================================================================
 # WADcoms link
 # netexec
+pipx install git+https://github.com/Pennyw0rth/NetExec
+
 # bloodhound
-# impacket
-#   psexec.py
+
+# Impacket
+mkdir ~/WindowsTools/impacket
+python3 -m pipx install impacket
+git clone https://github.com/dirkjanm/krbrelayx.git ~/WindowsTools/impacket
+
 # responder
+mkdir ~/WindowsTools/responder
+git clone https://github.com/lgandx/Responder.git ~/WindowsTools/responder
+
 # enum4linux
 # ldapdomaindump
 # ldapsearch
-# Kerbrute
 # smbmap
 # windapsearch
 # shortscan - IIS Scanner + ds_walk
 # https://github.com/ShutdownRepo/targetedKerberoast
-cd ~/WindowsTools
-sudo git clone https://github.com/ropnop/kerbrute.git
-cd kerbrute
-sudo make all
+
+# Kerbrute
+mkdir ~/WindowsTools/kerbrute
+sudo git clone https://github.com/ropnop/kerbrute.git ~/WindowsTools/kerbrute
+sudo make -C ~/WindowsTools/kerbrute all
 sudo ln -s ~/WindowsTools/kerbrute/dist/kerbrute_linux_amd64 /usr/local/bin/kerbrute
 
 #===============================================================================
 # PIVOTING TOOLING =============================================================
 #===============================================================================
 #ligolo
+mkdir ~/PivotingTools/Ligolo
+wget -P ~/PivotingTools/Ligolo https://github.com/nicocha30/ligolo-ng/releases/download/v0.8.2/ligolo-ng_proxy_0.8.2_linux_amd64.tar.gz
+tar -xvzf ~/PivotingTools/Ligolo/ligolo-ng_proxy_0.8.2_linux_amd64.tar.gz -C ~/PivotingTools/Ligolo
+
 #chisel
 
 #===============================================================================
