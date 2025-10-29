4af984ad13a766d7fa0d4a4cc0173f2a998fa255
3mi55ary <Gapedwards4@gmail.com>
2025-10-29 00:05:04 -0400

    Update StoreHostname.sh

--- StoreHostname.sh
+++ StoreHostname.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter ip: " ip
-read -p "Enter hostname: " hostname
+read -p "Enter hostname(s) seperated by a space: " hostname
 echo
 # Write to /etc/hosts
 echo "$ip	$hostname" | sudo tee -a /etc/hosts > /dev/null
