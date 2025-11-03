004475d841e3adafc2fbe0f851f187f324575e5d
3mi55ary <Gapedwards4@gmail.com>
2025-11-03 15:52:07 -0500

    Revise script description and usage for Alpine Router
    Updated script description and usage notes to reflect changes.

---
+++ ENVAlpineRouter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#===============================================================================
+# SCRIPT NAME   : ENVAlpineRouter.sh
+# DESCRIPTION   : Builds a hardened Alpine Router
+# AUTHOR        : 3mi55ary
+# DATE          : 2025-11-2
+# VERSION       : 1.0
+# USAGE         : ./ENVAlpineRouter.sh
+# NOTES         : 
+# NOTES         : Tested on Latest Release of Alpine Linux.
+#===============================================================================
